7f6deb12af8f612698845b0bd16f494e10ed4982
Anton Lebedev <lebaster@gmail.com>
2018-07-31 09:00:38 +0600

    Fix the mongodump command line options syntax.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -8,19 +8,19 @@ DROPBOX_UPLOADER=/opt/dropbox_uploader/dropbox_uploader.sh
 if ( [ -z "${MONGODB_USERNAME}" ] || [ -z "${MONGODB_PASSWORD}" ] );
 then
   /usr/bin/mongodump \
-  --host $MONGODB_HOST \
-  --port $MONGODB_PORT \
-  --db $MONGODB_DATABASE \
+  --host=$MONGODB_HOST \
+  --port=$MONGODB_PORT \
+  --db=$MONGODB_DATABASE \
   --gzip \
   --archive=$DESTINATION_PATH/$DATE.gz
 else
   /usr/bin/mongodump \
-  --host $MONGODB_HOST \
-  --port $MONGODB_PORT \
-  --db $MONGODB_DATABASE \
-  --username $MONGODB_USERNAME \
-  --password $MONGODB_PASSWORD \
-  --authenticationDatabase $MONGODB_AUTH_DATABASE \
+  --host=$MONGODB_HOST \
+  --port=$MONGODB_PORT \
+  --db=$MONGODB_DATABASE \
+  --username=$MONGODB_USERNAME \
+  --password=$MONGODB_PASSWORD \
+  --authenticationDatabase=$MONGODB_AUTH_DATABASE \
   --gzip \
   --archive=$DESTINATION_PATH/$DATE.gz
 fi
