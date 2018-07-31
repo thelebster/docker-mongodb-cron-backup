48789f05ac3d5b91abf61a85d1c9c7185c705b3c
Anton Lebedev <lebaster@gmail.com>
2018-07-31 13:06:46 +0600

    Fix env vars for cron.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 set -e
 
-BASH_PROFILE=/root/.bash_profile
+# BASH_PROFILE=/root/.bash_profile
 
-touch $BASH_PROFILE
-echo "export MONGODB_HOST=${MONGODB_HOST}" >> $BASH_PROFILE
-echo "export MONGODB_PORT=${MONGODB_PORT}" >> $BASH_PROFILE
-echo "export MONGODB_DATABASE=${MONGODB_DATABASE}" >> $BASH_PROFILE
-echo "export MONGODB_USERNAME=${MONGODB_USERNAME}" >> $BASH_PROFILE
-echo "export MONGODB_PASSWORD=${MONGODB_PASSWORD}" >> $BASH_PROFILE
-echo "export MONGODB_AUTH_DATABASE=${MONGODB_AUTH_DATABASE}" >> $BASH_PROFILE
-echo "export DESTINATION_PATH=${DESTINATION_PATH}" >> $BASH_PROFILE
+# touch $BASH_PROFILE
+# echo "export MONGODB_HOST=${MONGODB_HOST}" >> $BASH_PROFILE
+# echo "export MONGODB_PORT=${MONGODB_PORT}" >> $BASH_PROFILE
+# echo "export MONGODB_DATABASE=${MONGODB_DATABASE}" >> $BASH_PROFILE
+# echo "export MONGODB_USERNAME=${MONGODB_USERNAME}" >> $BASH_PROFILE
+# echo "export MONGODB_PASSWORD=${MONGODB_PASSWORD}" >> $BASH_PROFILE
+# echo "export MONGODB_AUTH_DATABASE=${MONGODB_AUTH_DATABASE}" >> $BASH_PROFILE
+# echo "export DESTINATION_PATH=${DESTINATION_PATH}" >> $BASH_PROFILE
+
+sed -i 's/$MONGODB_HOST/${MONGODB_HOST}/g' /opt/scripts/backup.sh
+sed -i 's/$MONGODB_PORT/${MONGODB_PORT}/g' /opt/scripts/backup.sh
+sed -i 's/$MONGODB_DATABASE/${MONGODB_DATABASE}/g' /opt/scripts/backup.sh
+sed -i 's/$DESTINATION_PATH/${DESTINATION_PATH}/g' /opt/scripts/backup.sh
 
 if ( [ -z "${OAUTH_ACCESS_TOKEN}" ] );
 then
